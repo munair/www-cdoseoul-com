6ffe6627f9bd910f876772628de1abc36e9b3882
Munair Simpson <munair@gmail.com>
2015-04-14 06:39:25 +0000

    including videos, blog, and registration. news still to be added. also needs exhaustive vetting.

--- web.js
+++ web.js
@@ -9,8 +9,26 @@ app.use("/img", express.static(__dirname + '/img'));
 app.use("/js", express.static(__dirname + '/js'));
 
 app.get('/', function(request, response) { var htmlBuffer = fs.readFileSync('index.html', 'utf-8'); response.send(htmlBuffer); });
-app.get('/news', function(request, response) { var htmlBuffer = fs.readFileSync('news.html', 'utf-8'); response.send(htmlBuffer); });
-app.get('/blog', function(request, response) { var htmlBuffer = fs.readFileSync('blog.html', 'utf-8'); response.send(htmlBuffer); });
+app.get('/videos', function(request, response) { var htmlBuffer = fs.readFileSync('index.html', 'utf-8'); response.send(htmlBuffer); });
+app.get('/news', function(request, response) { var htmlBuffer = fs.readFileSync('index.html', 'utf-8'); response.send(htmlBuffer); });
+app.get('/blog', function(request, response) { var htmlBuffer = fs.readFileSync('index.html', 'utf-8'); response.send(htmlBuffer); });
+app.get('/register', function(request, response) { var htmlBuffer = fs.readFileSync('index.html', 'utf-8'); response.send(htmlBuffer); });
+app.get('/contact', function(request, response) { var htmlBuffer = fs.readFileSync('index.html', 'utf-8'); response.send(htmlBuffer); });
+app.get('/getsmarter', function(request, response) { var htmlBuffer = fs.readFileSync('index.html', 'utf-8'); response.send(htmlBuffer); });
+app.get('/expatsintegrate', function(request, response) { var htmlBuffer = fs.readFileSync('index.html', 'utf-8'); response.send(htmlBuffer); });
+app.get('/classpreparation', function(request, response) { var htmlBuffer = fs.readFileSync('index.html', 'utf-8'); response.send(htmlBuffer); });
+
+app.get('/inc_.html', function(request, response) { var htmlBuffer = fs.readFileSync('inc_.html', 'utf-8'); response.send(htmlBuffer); });
+app.get('/inc_videos.html', function(request, response) { var htmlBuffer = fs.readFileSync('inc_videos.html', 'utf-8'); response.send(htmlBuffer); });
+app.get('/inc_news.html', function(request, response) { var htmlBuffer = fs.readFileSync('inc_news.html', 'utf-8'); response.send(htmlBuffer); });
+app.get('/inc_blog.html', function(request, response) { var htmlBuffer = fs.readFileSync('inc_blog.html', 'utf-8'); response.send(htmlBuffer); });
+app.get('/inc_register.html', function(request, response) { var htmlBuffer = fs.readFileSync('inc_register.html', 'utf-8'); response.send(htmlBuffer); });
+app.get('/inc_contact.html', function(request, response) { var htmlBuffer = fs.readFileSync('inc_contact.html', 'utf-8'); response.send(htmlBuffer); });
+app.get('/inc_getsmarter.html', function(request, response) { var htmlBuffer = fs.readFileSync('inc_getsmarter.html', 'utf-8'); response.send(htmlBuffer); });
+app.get('/inc_expatsintegrate.html', function(request, response) { var htmlBuffer = fs.readFileSync('inc_expatsintegrate.html', 'utf-8'); response.send(htmlBuffer); });
+app.get('/inc_classpreparation.html', function(request, response) { var htmlBuffer = fs.readFileSync('inc_classpreparation.html', 'utf-8'); response.send(htmlBuffer); });
+
+app.get('/inc_email.html', function(request, response) { var htmlBuffer = fs.readFileSync('inc_email.html', 'utf-8'); response.send(htmlBuffer); });
 
 var port = process.env.PORT || 8080;
 app.listen(port, function() {
